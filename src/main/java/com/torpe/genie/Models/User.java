0c03853984b15cec5773649158a59a3a8e3bea98
JoaoTorpe <victotorpe10@gmail.com>
2024-05-30 20:21:09 -0300

    UserDetails methods

--- src/main/java/com/torpe/genie/Models/User.java
+++ src/main/java/com/torpe/genie/Models/User.java
@@ -4,18 +4,24 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "user_table")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "EMAIL", nullable = false)
     private String email;
-
+    @Column(name = "PASSWORD",nullable = false)
     private String password;
+    @Column(name = "NAME",nullable = false)
     private String name;
 
     public User(String email , String password,String name){
@@ -25,6 +31,41 @@ public class User {
         this.name = name;
 
 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
