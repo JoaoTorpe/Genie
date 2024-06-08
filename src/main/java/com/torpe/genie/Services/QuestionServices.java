7f72057a1820e6eaeadb807a7b5667c5cea3d8b9
JoaoTorpe <victotorpe10@gmail.com>
2024-06-07 22:07:56 -0300

    Question service

--- src/main/java/com/torpe/genie/Services/QuestionServices.java
+++ src/main/java/com/torpe/genie/Services/QuestionServices.java
@@ -1,2 +1,46 @@
-package com.torpe.genie.Services;public class QuestionServices {
+package com.torpe.genie.Services;
+
+import com.torpe.genie.DTOs.GetUserDTO;
+import com.torpe.genie.DTOs.QuestionDTO;
+import com.torpe.genie.Models.Options;
+import com.torpe.genie.Models.Question;
+import com.torpe.genie.Models.User;
+import com.torpe.genie.Repositories.OptionsRepository;
+import com.torpe.genie.Repositories.QuestionRepository;
+import com.torpe.genie.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class QuestionServices {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    OptionsRepository optionsRepository;
+    @Autowired
+    JwtServices jwtServices;
+
+
+
+    public ResponseEntity createQuestion(QuestionDTO newQuestionData, User creator){
+
+        for (Options o : newQuestionData.alternativas()){
+            optionsRepository.save(o);
+        }
+
+        Question newQuestion = new Question(creator,newQuestionData.alternativas(),newQuestionData.materia(),newQuestionData.assunto(), newQuestionData.dica(),newQuestionData.resolucao(), newQuestionData.enunciado(), newQuestionData.dificuldade());
+        questionRepository.save(newQuestion);
+        return ResponseEntity.ok().body(newQuestion);
+
+    }
+
+
 }
