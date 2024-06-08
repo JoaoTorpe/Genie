08d03be62201f5ba594bd8f986ea80f9dd57e68f
JoaoTorpe <victotorpe10@gmail.com>
2024-06-07 22:06:01 -0300

    Options model and Repository

--- src/main/java/com/torpe/genie/Models/Options.java
+++ src/main/java/com/torpe/genie/Models/Options.java
@@ -1,2 +1,29 @@
-package com.torpe.genie.Models;public class Options {
+package com.torpe.genie.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Options {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     Long id;
+     String texto;
+     Boolean correta;
+
+     public Options(){}
+
+     public Options(String texto,Boolean correta){
+          this.texto = texto;
+          this.correta = correta;
+
+
+     }
 }
--- src/main/java/com/torpe/genie/Repositories/OptionsRepository.java
+++ src/main/java/com/torpe/genie/Repositories/OptionsRepository.java
@@ -1,2 +1,9 @@
-package com.torpe.genie.Repositories;public interface OptionsRepository {
+package com.torpe.genie.Repositories;
+
+import com.torpe.genie.Models.Options;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OptionsRepository extends JpaRepository<Options,Long> {
 }
