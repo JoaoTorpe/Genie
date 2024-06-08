f7e1092da285aaef3d11392c3e71304880145f54
JoaoTorpe <victotorpe10@gmail.com>
2024-06-07 22:01:09 -0300

    Question controller

--- src/main/java/com/torpe/genie/Controllers/QuestionController.java
+++ src/main/java/com/torpe/genie/Controllers/QuestionController.java
@@ -1,2 +1,41 @@
-package com.torpe.genie.Controllers;public class QuestionController {
+package com.torpe.genie.Controllers;
+
+import com.torpe.genie.DTOs.GetUserDTO;
+import com.torpe.genie.DTOs.QuestionDTO;
+import com.torpe.genie.Models.User;
+import com.torpe.genie.Services.JwtServices;
+import com.torpe.genie.Services.QuestionServices;
+import com.torpe.genie.Services.UserServices;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+    @Autowired
+    QuestionServices questionServices;
+
+    @Autowired
+    JwtServices jwtServices;
+
+    @Autowired
+    UserServices userServices;
+
+    @PostMapping
+    public ResponseEntity createQuestion(@RequestBody QuestionDTO questionData, HttpServletRequest request){
+
+                User creator =  userServices.currentUser(request);
+
+        return questionServices.createQuestion(questionData,creator);
+
+
+    }
+
 }
