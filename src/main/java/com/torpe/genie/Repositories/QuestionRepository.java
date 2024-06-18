301f315333795776e6026d70542e97a5a1ecde69
JoaoTorpe <victotorpe10@gmail.com>
2024-06-18 16:23:25 -0300

    Refactor: get all questions query

--- src/main/java/com/torpe/genie/Repositories/QuestionRepository.java
+++ src/main/java/com/torpe/genie/Repositories/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.torpe.genie.Repositories;
 
 import com.torpe.genie.Models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 
-    List<Question> findByCreator_id(Long creator_id);
+    @Query("SELECT q FROM Question q WHERE q.creator.id = :creatorId ORDER BY q.id DESC")
+    List<Question> findByCreator_id(@Param("creatorId") Long creatorId);
 }
